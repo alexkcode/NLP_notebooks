bc381cf360efa82bda894731e5e2a572d94b3ee8
Alexis Kwan <alexiskwan.uiuc@gmail.com>
2017-01-18 18:00:42 -0700

    env things

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'NLP Notebooks',
+    'author': 'Alexis K.',
+    'url': '',
+    'download_url': '',
+    'author_email': '',
+    'version': '0.1',
+    'install_requires': ['nose, nltk'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'nlp_notebooks'
+}
+
+setup(**config)
